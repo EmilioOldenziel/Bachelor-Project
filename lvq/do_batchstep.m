0b134b36b4f999f95c23ecfbba6c5c156b887a85
EmilioOldenziel <emilio.oldenziel@gmail.com>
2017-05-16 16:54:04 +0200

    batch step

--- lvq/do_batchstep.m
+++ lvq/do_batchstep.m
@@ -43,7 +43,6 @@ end
 % omat=sqrtm(lambda);
 prot=proti;                            % prototypes before step
 
-
      chp = 0*prot; chm = 0*omat;       % initialize change of prot,omega
      
      for i= 1:nfv;                     % loop through all training examples
@@ -52,7 +51,7 @@ prot=proti;                            % prototypes before step
      % calculate squared distances to all prototypes
        dist = nan(np,1);                 % define squared distances
        for j=1:np;                       % distances from all prototypes
-         dist(j) = (norm( omat*(fvi-prot(j,:))' ))^2;
+         dist(j) = (norm( omat{j}*(fvi-prot(j,:))' ))^2;
        end;
    
     % find the two winning prototypes
@@ -69,59 +68,76 @@ prot=proti;                            % prototypes before step
      
       DJ = (fvi-wJ)';  DK = (fvi-wK)';        % displacement vectors 
       norm_factor = (dJ+dK)^2;                % denominator of prefactor
-      dwJ = -(dK/norm_factor)*lambda*DJ;      % change of correct winner
-      dwK =  (dJ/norm_factor)*lambda*DK;      % change of incorrect winner
+      dwJ = -(dK/norm_factor)*lambda{jwin}*DJ;      % change of correct winner
+      dwK =  (dJ/norm_factor)*lambda{kwin}*DK;      % change of incorrect winner
       
       % matrix update, single (global) matrix omat for one example
-	  f1 = ( dK/norm_factor)*(omat*DJ)*DJ';   % term 1 of matrix change
-	  f2 = (-dJ/norm_factor)*(omat*DK)*DK';   % term 2 of matrix change
+	    f1 = ( dK/norm_factor)*(omat{jwin}*DJ)*DJ';   % term 1 of matrix change
+	    f2 = (-dJ/norm_factor)*(omat{kwin}*DK)*DK';   % term 2 of matrix change
      
       % negative gradient update added up over examples
       chp(jwin,:) = chp(jwin,:) - dwJ';  % correct    winner summed update
       chp(kwin,:) = chp(kwin,:) - dwK';  % incorrect winner summed update
-	  chm = chm - (f1 + f2);             % matrix summed update
-   
+      if(mode~=4)
+	      chm = chm - (f1 + f2);             % matrix summed update
+      end
     end; % end of one loop through (sum over) all examples
 
-    % singularity control: add  derivative of penalty term times mu 
-    if(mu>0)
-        chm = chm + mu*pinv(omat');
-    end;
-    
-    % compute normalized gradient updates (length 1)
-    % separate nomralization for prototypes and the matrix
-    % computation of actual changes, diagonal matrix imposed here if nec.
-    n2chw = 0;               % zero initial value of sum
-    for ni=1:np;             % loop through (sum over) set of prototypes
-         n2chw = n2chw + dot(chp(ni,:),chp(ni,:)); 
-    end;                     % total length of concatenated prototype vec.
-   
-    if (mode==2)             % if diagonal matrix used only 
-        chm=diag(diag(chm)); % reduce to diagonal changes
-    end;
-    n2chm = sum(sum(chm.^2));% total 'length' of matrix update
+    if(mode~=4)
+      % singularity control: add  derivative of penalty term times mu 
+      if(mu>0)
+          chm = chm + mu*pinv(omat');
+      end;
       
-    % final, normalized gradient updates after 1 loop through examples
-    prot = prot  + etap * chp/sqrt(n2chw);
-    omat = omat  + etam * chm/sqrt(n2chm);
-       
+      % compute normalized gradient updates (length 1)
+      % separate nomralization for prototypes and the matrix
+      % computation of actual changes, diagonal matrix imposed here if nec.
+      n2chw = 0;               % zero initial value of sum
+      for ni=1:np;             % loop through (sum over) set of prototypes
+          n2chw = n2chw + dot(chp(ni,:),chp(ni,:)); 
+      end;                     % total length of concatenated prototype vec.
     
-    if (mode==2)                   % if diagonal matrix only 
-         omat = diag(diag(omat));  % reduce to diagonal matrix
-    end;                           % probably obsolete as chm diagonal
+      if (mode==2)             % if diagonal matrix used only 
+          chm=diag(diag(chm)); % reduce to diagonal changes
+      end;
+      n2chm = sum(sum(chm.^2));% total 'length' of matrix update
+        
+      % final, normalized gradient updates after 1 loop through examples
+      prot = prot  + etap * chp/sqrt(n2chw);
+      omat = omat  + etam * chm/sqrt(n2chm);
       
-    %  nullspace correction using Moore Penrose pseudo-inverse
-    if (mode==1)
-         xvec=[fvec;prot];               % concat. protos and fvecs
-         omat= ((omat*xvec')*pinv(xvec')); % corrected omega matrix 
-    end;
+      if (mode==2)                   % if diagonal matrix only 
+          omat = diag(diag(omat));  % reduce to diagonal matrix
+      end;                           % probably obsolete as chm diagonal
+        
+      %  nullspace correction using Moore Penrose pseudo-inverse
+      if (mode==1)
+          xvec=[fvec;prot];               % concat. protos and fvecs
+          omat= ((omat*xvec')*pinv(xvec')); % corrected omega matrix 
+      end;
+        
+      if (mode==3);            
+          omat=eye(ndim);  % reset to identity regardless of gradients
+      end;
       
-    if (mode==3);            
-         omat=eye(ndim);  % reset to identity regardless of gradients
-    end;
-    
-    % normalization of omega, corresponds to Trace(lambda) = 1
-    omat = omat / sqrt( sum(sum(omat.^2)) );  
+      % normalization of omega, corresponds to Trace(lambda) = 1
+      omat = omat / sqrt( sum(sum(omat.^2)) );
+    else
+      if (mu>0)
+        f1 = f1 * mu*pinv(omat{jwin})';
+        f2 = f2 * mu*pinv(omat{kwin})';
+      end
+
+      n2chw = 0;               % zero initial value of sum
+      for ni=1:np;             % loop through (sum over) set of prototypes
+          n2chw = n2chw + dot(chp(ni,:),chp(ni,:)); 
+      end;
+      n2chm = sum(sum(chm.^2));% total 'length' of matrix update
+      prot = prot  + etap * chp/sqrt(n2chw);
+
+      omat{jwin} = omat{jwin} + etam * f1/sqrt(n2chm);
+      omat{kwin} = omat{kwin} + etam * f2/sqrt(n2chm);
+    end
      
 end  % one full, normalized gradient step performed, return omat and prot
 
